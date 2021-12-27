5c151ab0961adc8e799b433040e88cb2cb961ecc
akashbg3010 <akashbpura@gmail.com>
2021-12-28 00:46:50 +0530

    Added PrimeNumbers

---
+++ src/com/bridgelabz/algorithmsprograms/PrimeNumbers.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.algorithmsprograms;
+
+import java.util.Scanner;
+
+public class PrimeNumbers {
+
+	public static void main(String[] args)
+	{
+		int lowerLimit,upperLimit;
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the lower limit: ");
+		lowerLimit= sc.nextInt(); 
+		System.out.println("Enter the upper limit : ");
+		upperLimit = sc.nextInt();
+		
+		PrimeNumbers range = new PrimeNumbers();
+		System.out.println("Prime numbers between the given range are:");
+		range.primeRange(lowerLimit,upperLimit);
+	}
+	public void primeRange(int lower,int upper) {
+		for (int i = lower; i <= upper; i++) {
+			if (i == 1 || i == 0)
+				continue;
+			int flag = 1;
+
+			for (int j = 2; j <= i / 2; j++) {
+				if (i % j == 0) {
+					flag = 0;
+					break;
+				}
+			}
+			if (flag == 1)
+				System.out.println(i);
+		}
+	}
+}
